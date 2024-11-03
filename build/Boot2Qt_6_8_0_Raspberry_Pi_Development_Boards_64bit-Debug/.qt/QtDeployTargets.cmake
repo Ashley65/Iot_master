598c5b3cc24155a5df11d2b68afa409553715afa
Ashley65 <nchinembiri@outlook.com>
2024-11-03 19:15:30 +0000

    adding project to git

---
+++ build/Boot2Qt_6_8_0_Raspberry_Pi_Development_Boards_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_master_FILE D:/Projects/C-C++ Projects/master/build/Boot2Qt_6_8_0_Raspberry_Pi_Development_Boards_64bit-Debug/master)
+set(__QT_DEPLOY_TARGET_master_TYPE EXECUTABLE)
